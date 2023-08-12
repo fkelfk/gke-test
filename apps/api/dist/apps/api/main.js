c3a125ba485d03ede3fcf742bc90dba86e213a86
fkelfk <fkelfk09@gmail.com>
2023-08-13 01:09:15 +0900

    test 0.1.0

--- apps/api/dist/apps/api/main.js
+++ apps/api/dist/apps/api/main.js
@@ -24,12 +24,11 @@ const tslib_1 = __webpack_require__(4);
 const common_1 = __webpack_require__(1);
 const app_controller_1 = __webpack_require__(5);
 const app_service_1 = __webpack_require__(6);
-const users_module_1 = __webpack_require__(7);
 let AppModule = exports.AppModule = class AppModule {
 };
 exports.AppModule = AppModule = tslib_1.__decorate([
     (0, common_1.Module)({
-        imports: [users_module_1.UsersModule],
+        imports: [],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
@@ -90,128 +89,6 @@ let AppService = exports.AppService = class AppService {
 exports.AppService = AppService = tslib_1.__decorate([
     (0, common_1.Injectable)()
 ], AppService);
-
-
-/***/ }),
-/* 7 */
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.UsersModule = void 0;
-const tslib_1 = __webpack_require__(4);
-const common_1 = __webpack_require__(1);
-const users_service_1 = __webpack_require__(8);
-const users_controller_1 = __webpack_require__(9);
-let UsersModule = exports.UsersModule = class UsersModule {
-};
-exports.UsersModule = UsersModule = tslib_1.__decorate([
-    (0, common_1.Module)({
-        controllers: [users_controller_1.UsersController],
-        providers: [users_service_1.UsersService],
-    })
-], UsersModule);
-
-
-/***/ }),
-/* 8 */
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.UsersService = void 0;
-const tslib_1 = __webpack_require__(4);
-const common_1 = __webpack_require__(1);
-let UsersService = exports.UsersService = class UsersService {
-    create(createUserDto) {
-        return 'This action adds a new user';
-    }
-    findAll() {
-        return `This action returns all users`;
-    }
-    findOne(id) {
-        return `This action returns a #${id} user`;
-    }
-    remove(id) {
-        return `This action removes a #${id} user`;
-    }
-};
-exports.UsersService = UsersService = tslib_1.__decorate([
-    (0, common_1.Injectable)()
-], UsersService);
-
-
-/***/ }),
-/* 9 */
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-
-var _a, _b;
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.UsersController = void 0;
-const tslib_1 = __webpack_require__(4);
-const common_1 = __webpack_require__(1);
-const users_service_1 = __webpack_require__(8);
-const create_user_dto_1 = __webpack_require__(10);
-let UsersController = exports.UsersController = class UsersController {
-    constructor(usersService) {
-        this.usersService = usersService;
-    }
-    create(createUserDto) {
-        return this.usersService.create(createUserDto);
-    }
-    findAll() {
-        return this.usersService.findAll();
-    }
-    findOne(id) {
-    }
-    remove(id) {
-        return this.usersService.remove(+id);
-    }
-};
-tslib_1.__decorate([
-    (0, common_1.Post)(),
-    tslib_1.__param(0, (0, common_1.Body)()),
-    tslib_1.__metadata("design:type", Function),
-    tslib_1.__metadata("design:paramtypes", [typeof (_b = typeof create_user_dto_1.CreateUserDto !== "undefined" && create_user_dto_1.CreateUserDto) === "function" ? _b : Object]),
-    tslib_1.__metadata("design:returntype", void 0)
-], UsersController.prototype, "create", null);
-tslib_1.__decorate([
-    (0, common_1.Get)(),
-    tslib_1.__metadata("design:type", Function),
-    tslib_1.__metadata("design:paramtypes", []),
-    tslib_1.__metadata("design:returntype", void 0)
-], UsersController.prototype, "findAll", null);
-tslib_1.__decorate([
-    (0, common_1.Get)(':id'),
-    tslib_1.__param(0, (0, common_1.Param)('id')),
-    tslib_1.__metadata("design:type", Function),
-    tslib_1.__metadata("design:paramtypes", [String]),
-    tslib_1.__metadata("design:returntype", void 0)
-], UsersController.prototype, "findOne", null);
-tslib_1.__decorate([
-    (0, common_1.Delete)(':id'),
-    tslib_1.__param(0, (0, common_1.Param)('id')),
-    tslib_1.__metadata("design:type", Function),
-    tslib_1.__metadata("design:paramtypes", [String]),
-    tslib_1.__metadata("design:returntype", void 0)
-], UsersController.prototype, "remove", null);
-exports.UsersController = UsersController = tslib_1.__decorate([
-    (0, common_1.Controller)('users'),
-    tslib_1.__metadata("design:paramtypes", [typeof (_a = typeof users_service_1.UsersService !== "undefined" && users_service_1.UsersService) === "function" ? _a : Object])
-], UsersController);
-
-
-/***/ }),
-/* 10 */
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.CreateUserDto = void 0;
-class CreateUserDto {
-}
-exports.CreateUserDto = CreateUserDto;
 
 
 /***/ })
