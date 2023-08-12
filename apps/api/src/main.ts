527bacf03226f5c18b0d3b0616c64d6c9ba23d8b
fkelfk <fkelfk09@gmail.com>
2023-08-13 01:34:23 +0900

    test 0.1.2

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,14 +7,27 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  app.use(
+    '/api/user',
+    createProxyMiddleware({
+      target: 'http://user:3300',
+      changeOrigin: true,
+      secure: false,
+    })
+  );
+
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
